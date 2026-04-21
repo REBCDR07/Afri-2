c4c170dbd8ae65850bef729faa8f2862db00d0fc
Elton Ronald Bill Hounnou <eltonhounnou2@gmail.com>
2026-04-22 00:38:49 +0100

    Add My AfriChat widget to main.ts
    Import My AfriChat and mount the widget on the portfolio.

---
+++ src/main.ts
@@ -0,0 +1,5 @@
+import { createMyAfriChat } from "my-africhat";
+import afriChatConfig from "./africhat.config";
+
+// Mount My AfriChat widget on the portfolio
+createMyAfriChat(afriChatConfig).mount();
